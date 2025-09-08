5457d46e5500f23c74378839861bb45e2f98a84c
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-09 00:44:57 +0900

    Add: lecture-1

---
+++ lecture-1/src/App.js
@@ -0,0 +1,23 @@
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+// import ListPage from './pages/ListPage/index';
+// import ViewPage from './pages/ViewPage/index';
+
+const ListPage = lazy(() => import('./pages/ListPage/index'));
+const ViewPage = lazy(() => import('./pages/ViewPage/index'));
+
+function App() {
+  return (
+    <div className="App">
+      <Suspense fallback={<div>로딩중...</div>}>
+        <Routes>
+          <Route path="/" element={<ListPage />} />
+          <Route path="/view/:id" element={<ViewPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
